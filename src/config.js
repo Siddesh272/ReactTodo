7a0db8733dffb7989b44df85790042ce96e60ee1
Siddesh272 <90123105+Siddesh272@users.noreply.github.com>
2022-10-05 12:13:19 +0530

    make it better

---
+++ src/config.js
@@ -0,0 +1,13 @@
+
+const firebaseConfig = {
+  apiKey: "AIzaSyALZTptijHEySdZfvJzC2B_13Mo825crvU",
+  authDomain: "todofirebase-d08d8.firebaseapp.com",
+  projectId: "todofirebase-d08d8",
+  storageBucket: "todofirebase-d08d8.appspot.com",
+  messagingSenderId: "753110622459",
+  appId: "1:753110622459:web:05ca687f06ee9c5eea905e"
+};
+
+// Initialize Firebaseconst app = initializeApp(firebaseConfig);
+
+export default firebaseConfig
